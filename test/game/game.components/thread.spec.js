9965ebed95b545fa3aea97f09706aa694ebf0fe7
Isaac Madwed <i.madwed@gmail.com>
2015-08-22 10:13:48 -0400

    added test folder, mocha will now test using npm test

---
+++ test/game/game.components/thread.spec.js
@@ -0,0 +1,19 @@
+// "use strict";
+// var requirejs = require("requirejs");
+
+// requirejs.config({
+// 	baseUrl: "../../../../",
+// 	nodeRequire: require
+// });
+
+// describe("Moves", function () {
+// 	var thread;
+
+// 	setup(function (done) {
+// 		requirejs(["../../../../browser/game/game.components/thread"], function (modu) {
+// 			thread = modu;
+// 			done();
+// 		});
+// 	});	
+
+// });
